c5952eca38aa5964405d377f97dfab090d2ccbcd
Miles Sabin <miles@milessabin.com>
2021-03-17 17:31:43 +0000

    Switch to github actions

--- run.sh
+++ run.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-BRANCH="$1"
+SHAPELESS_BRANCH="$1"
 
 DOTTY_MAJOR_VERSION=`wget -q -O - https://dotty.epfl.ch/versions/latest-nightly-base`
-DOTTY_VERSION=`wget -q -O - https://repo1.maven.org/maven2/ch/epfl/lamp/dotty_${DOTTY_MAJOR_VERSION}/maven-metadata.xml | grep "<version>" | /usr/bin/tail -1 | cut -d">" -f2 | cut -d"<" -f1`
+DOTTY_VERSION=`wget -q -O - https://repo1.maven.org/maven2/org/scala-lang/scala3-compiler_${DOTTY_MAJOR_VERSION}/maven-metadata.xml | grep "<version>" | /usr/bin/tail -1 | cut -d">" -f2 | cut -d"<" -f1`
 
-echo "Branch: $BRANCH"
 echo "Dotty version: $DOTTY_VERSION"
+echo "shapeless branch: $SHAPELESS_BRANCH"
+
+case $SHAPELESS_BRANCH in
+  shapeless-3)
+    TESTS="test"
+    ;;
+  *)
+    echo "Unknown shapeless branch $SHAPELESS_BRANCH"
+    exit 1
+esac
+
+echo "Tests:" $TESTS
 
 rm -rf shapeless/ && \
 git clone https://github.com/milessabin/shapeless.git && \
 cd shapeless && \
-git checkout shapeless-3 && \
-sbt ++$DOTTY_VERSION "clean ; compile ; test:compile ; test"
+git checkout $SHAPELESS_BRANCH && \
+sbt ++$DOTTY_VERSION $TESTS
